ebbfe6bf83befc9cbd3f50f8ccf2dbc440186523
BigYoplait <48740378+BigYoplait@users.noreply.github.com>
2022-12-02 11:10:24 -0500

    Create patient.py
    patient.py utilizes the print function, creates a few variables, and uses the input function to take user input from the terminal. Basic ideas, could be fleshed out more to pass variables to a object constructor

---
+++ patient.py
@@ -0,0 +1,6 @@
+print("Welcome: Create a new Patient")
+name = input("What is the patient's name? ")
+age = input("What is the patient's age? ")
+isNew = input("Is this a new patient? (True/False) ")
+
+correct = input("Name: " + name + "\nAge: " + age + "\nExisting Patient: " + isNew + "\nDoes this look correct? (True/False) ")

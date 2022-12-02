bef14389b1a568508fd28e2afc3cf21a6bb71a01
BigYoplait <48740378+BigYoplait@users.noreply.github.com>
2022-12-02 10:55:10 -0500

    Updated hello.py
    Wrote in variables. Most of these are intuitive, just remember capitilization of False and True for booleans

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
+#this is a String
 msg = "Hello World"
+#this is also a String
+msg2 = 'Hello World'
+#this is an integer
+age = 1
+#this is a double
+cost = 10.95
+#this is a boolean
+isWhat = False
+isWhat = True
+
 print(msg)
